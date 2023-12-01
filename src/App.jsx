eb7805b593344852ad7e7796077ada1bfa1356db
Sabrina Hill <brinalynn@hotmail.com>
2023-12-01 07:41:43 +0000

    updated header & hero

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className='container'>
     <div><Header /></div>
-    <div><Navbar /></div>
     <div> <Hero/> </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,17 @@
 
 export function Hero() {
   return (
-    <div className="bg-blue-600 text-black h-screen w-screen flex items-center justify-center">
+    <div className="bg-white-600 text-black h-screen w-screen flex items-center justify-center">
       <div className="text-center">
-        <h1 className="text-8xl font-bold mb-4">Welcome!</h1>
+        <h1 className="text-8xl font-bold mb-4">Welcome To!</h1>
         <p className="text-lg"></p>
       </div>
+      <div></div>
       <div>
-        <a href=""></a>
+        <img src="/public/assets/beamused.png"/>
       </div>
-          
-         
-    </div>
+       </div>
+
+       
   );
 }
