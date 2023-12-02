47312f42ac3856cbfa0822a0f5a3daa09a50ead5
MarcosSanPedro <143010406+MarcosSanPedro@users.noreply.github.com>
2023-12-02 01:14:00 +0000

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { LoginPage } from './pages/LoginPage'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { SignupPage } from './pages/SignupPage';
-import { MainPage } from './pages/MainPage';
-import { Header2 } from './components/Header2';
-import { Events } from './components/Events';
+import { Header } from './components/Header';
+import { EventsPage } from './pages/EventsPage';
 
-import Chrismas from '../public/assets/Christmas-Events.jpg'
-
-
+ 
 function App() {
   
 
@@ -17,8 +14,8 @@ function App() {
      <Routes>
       <Route path='/' element={<LoginPage/>} />
       <Route path='/SignupPage' element={<SignupPage/>} />
-      <Route path='/MainPage' element={<Header2/>} />
-      <Route path='/EventsPage' element={ <Events image={Chrismas} tittle='Hola' description='hi' badges={['hola', 'hi']} />} /> 
+      <Route path='/MainPage' element={<Header/>}  />
+      <Route path='/EventsPage' element={ <EventsPage/>}  /> 
      </Routes>
      </Router>
      
--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,9 +1,18 @@
-import { Header2 } from "../components/Header2";
+import { Events } from "../components/Events";
+import { Header } from "../components/Header";
+import Chrismas from '../../public/assets/Christmas-Events.jpg'
 
 export function EventsPage(){
     return(
+        
         <div>
-            <Header2/>
+            <Header/>
+            <div className="grid grid-cols-1 m-auto w-fit mt-6 gap-5 md:grid-cols-2 xl:grid-cols-3">
+            <Events image={Chrismas} tittle='Hola' description='sa' time='Friday 27, 9:00pm-12:am' badges={['hola', 'hi']}/>
+            <Events image={Chrismas} tittle='Hola' description='hi' badges={['hola', 'hi']}/>
+            <Events image={Chrismas} tittle='Hola' description='hi' badges={['hola', 'hi']}/>
+            <Events image={Chrismas} tittle='Hola' description='hi' badges={['hola', 'hi']}/>
+            </div>
         </div>
     )
 }
