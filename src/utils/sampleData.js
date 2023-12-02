45af932634788cfe9fc24080df5af1774de69683
Mr. Mendez <mrmendez@amvestor.com>
2023-12-02 16:32:34 -0500

    Add API helper functions and update EventsPage component

---
+++ src/utils/sampleData.js
@@ -0,0 +1,138 @@
+export const themParks = [
+	{
+		name: "Disney World",
+		city: "Orlando",
+		state: "Florida",
+		country: "USA",
+		opened: 1971,
+		size: "25,000 acres",
+		description:
+			"Disney World is a theme park resort in Orlando, Florida. It is the largest theme park in the world, covering 25,000 acres of land. It is owned and operated by The Walt Disney Company through its Parks, Experiences and Products division.",
+		image: "https://images.unsplash.com/photo-1575089776834-8be34696ffb9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		rides: [
+			{
+				name: "Space Mountain",
+				opened: 1975,
+				description:
+					"Space Mountain is an indoor dark high-speed outer space-themed steel dual tracked enclosed roller coaster in Tomorrowland at the Magic Kingdom theme park at the Walt Disney World Resort in Bay Lake, Florida just outside of Orlando, Florida.",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+			{
+				name: "Splash Mountain",
+				opened: 1992,
+				description:
+					"Splash Mountain is a log flume at Disneyland, Tokyo Disneyland, and the Magic Kingdom at the Walt Disney World Resort, based on the characters, stories, and songs from the 1946 Disney film Song of the South.",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+			{
+				name: "Big Thunder Mountain Railroad",
+				opened: 1980,
+				description:
+					"Big Thunder Mountain Railroad is a mine train roller coaster located in Frontierland at several Disneyland-style Disney Parks worldwide. The ride exists at Disneyland Park (California) and the Magic Kingdom at Walt Disney World as Big Thunder Mountain Railroad, and at Tokyo Disneyland and Disneyland Park (Paris) as Big Thunder Mountain.",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+		],
+	},
+	{
+		name: "Universal Studios",
+		city: "Orlando",
+		state: "Florida",
+		country: "USA",
+		opened: 1990,
+		size: "541 acres",
+		description:
+			"Universal Studios Florida is a theme park and production studio located in Orlando, Florida, United States. Opened on June 7, 1990, the park's theme is the entertainment industry, in particular movies and television.",
+		image: "https://i.imgur.com/0qXbT8W.jpg",
+		rides: [
+			{
+				name: "Revenge of the Mummy",
+				opened: 2004,
+				description:
+					"Revenge of the Mummy is an enclosed roller coaster located at Universal Studios Florida, Universal Studios Hollywood, and Universal Studios Singapore. The ride is themed after the popular Mummy movie franchise directed by Stephen Sommers, which was a loose remake of the original 1932 film.",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+			{
+				name: "Hollywood Rip Ride Rockit",
+				opened: 2009,
+				description:
+					"Hollywood Rip Ride Rockit is a steel roller coaster at Universal Studios Florida in Orlando, Florida. With a height of 167 feet (51 m), a length of 3,800 feet (1,200 m), and a top speed of 65 miles per hour (105 km/h), it is the largest X-Coaster ever built by German manufacturer Maurer Söhne.",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+			{
+				name: "Harry Potter and the Escape from Gringotts",
+				opened: 2014,
+				description:
+					"Harry Potter and the Escape from Gringotts is an indoor steel roller coaster at Universal Studios Florida, a theme park located within the Universal Orlando Resort. The ride, which is themed to the Gringotts Wizarding Bank, became the flagship attraction for the expanded Wizarding World of Harry Potter when it opened on July 8, 2014.",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+		],
+	},
+	{
+		name: "Six Flags",
+		city: "Austell",
+		state: "Georgia",
+		country: "USA",
+		opened: 1967,
+		size: "290 acres",
+		description:
+			"Six Flags Over Georgia is a 290-acre (120 ha) theme park located west of Atlanta in Cobb County, Georgia, United States. Featuring characters and themes from a variety of Warner Bros. properties such as Looney Tunes and DC Comics, the park opened to the public in 1967 as the second of three theme parks built by Six Flags.",
+		image: "https://i.imgur.com/0qXbT8W.jpg",
+		rides: [
+			{
+				name: "Goliath",
+				opened: 2006,
+				description:
+					"Goliath is a steel hypercoaster located at Six Flags Over Georgia in Cobb County, Georgia, United States. Designed by Bolliger & Mabillard (B&M), the roller coaster reaches a maximum height of 200 feet (61 m) and a top speed of 70 miles per hour (110 km/h).",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+			{
+				name: "Batman: The Ride",
+				opened: 1997,
+				description:
+					"Batman: The Ride is a steel inverted roller coaster based thematically off of the 1989 film Batman and found at seven Six Flags theme parks in the United States. Built by consulting engineers Bolliger & Mabillard, it rises to a height of between 100 and 105 feet (30 and 32 m) and reaches top speeds of 50 miles per hour (80 km/h).",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+			{
+				name: "Mind Bender",
+				opened: 1978,
+				description:
+					"Mind Bender is a steel roller coaster located at Six Flags Over Georgia. Built in-house by Six Flags Over Georgia, it opened in 1978 and was the first triple-loop roller coaster in the world. It is also notable for being one of the few roller coasters in the world to feature a dive loop.",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+		],
+	},
+	{
+		name: "Busch Gardens",
+		city: "Tampa",
+		state: "Florida",
+		country: "USA",
+		opened: 1959,
+		size: "335 acres",
+		description:
+			"Busch Gardens Tampa is a 335-acre (136 ha) African-themed animal theme park located in the city of Tampa, Florida. Busch Gardens Tampa is owned and operated by SeaWorld Parks & Entertainment and is their 2nd largest park in terms of attendance behind SeaWorld Orlando.",
+		image: "https://i.imgur.com/0qXbT8W.jpg",
+		rides: [
+			{
+				name: "SheiKra",
+				opened: 2005,
+				description:
+					"SheiKra is a steel Dive Coaster roller coaster at the Busch Gardens Tampa Bay amusement park in Tampa, Florida, United States. The roller coaster was proposed by Mark Rose, vice-president of design and engineering for the park, and designed by Bolliger & Mabillard (B&M).",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+			{
+				name: "Cheetah Hunt",
+				opened: 2011,
+				description:
+					"Cheetah Hunt is a launched roller coaster at Busch Gardens Tampa Bay. Manufactured by Intamin, the ride opened on May 27, 2011, alongside a new cheetah exhibit in the Nairobi area of the park. The roller coaster is the first launched roller coaster to feature three separate LSM launches, and the second Intamin LSM launched roller coaster to feature a linear synchronous motor launch, following Maverick at Cedar Point.",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+			{
+				name: "Kumba",
+				opened: 1993,
+				description:
+					"Kumba is a steel roller coaster located at Busch Gardens Tampa Bay in Tampa, Florida. Manufactured by Bolliger & Mabillard, the ride opened in 1993. The ride has a layout similar to a figure 8, with the first half of the ride consisting of many loops and the second half consisting of many turns.",
+				image: "https://i.imgur.com/0qXbT8W.jpg",
+			},
+		],
+	},
+];
