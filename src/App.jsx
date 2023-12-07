5c222f3d9ea2996386ae21e07ee3c2f351a85f51
Sabrina Hill <brinalynn@hotmail.com>
2023-12-07 01:09:02 +0000

    login & landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { Hero } from './components/Hero';
-import { Header } from './components/Header/Header';
+
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { AuthContextProvider } from './context/AuthContext';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  
   return (
-    <div className='container'>
-    <div> <Header /> </div>
-    <div> <Hero /> </div>
-    </div>
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Navbar from './components/NavBarV1.jsx'
-import { Hero } from './components/Hero.jsx'
-import { Header } from './components/Header/Header.jsx'
-import { getWeather } from './components/Api/Weather.jsx'
-const apiKey = import.meta.env.VITE_OPEN_WEATHER_KEY;
-const city = "miami"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-
-const router = createBrowserRouter([
-  {path: "/", element: <App/>},
-  {path: "/Header", element: <Header/>, loader: getWeather(city, apiKey)},
-  {path: "/NavBar", element: <Navbar/>},
-  {path: "/Hero", element: <Hero/>},
-
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-    
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
