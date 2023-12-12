bc9bbcb604a1ee8ba4898950e2a6ca709cc11929
MarcosSanPedro <143010406+MarcosSanPedro@users.noreply.github.com>
2023-12-12 23:47:31 +0000

    commit

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -28,28 +28,32 @@ const Recommendation = () => {
   const handleDislikeClick = () => {
     loadRecommendation();
   };
+ 
 
   return (
-    <div>
+    <div className='w w-4/5 m-auto border border-black text-white bg-slate-600'>
       {media ? (
-        <div>
-          <img
+        <div className='flex gap-10'>
+          <img className='h h-72'
             src={`https://image.tmdb.org/t/p/w500${media.poster_path}`}
             alt={media.title || media.name}
           />
-          <h2>{media.title || media.name}</h2>
-          <p>{media.overview}</p>
           <div>
+          <h2>{media.title || media.name}</h2>
+          <p className='line line-clamp-3'>{media.overview}</p>
+          <div className='flex gap-6'>
             <button onClick={handleLikeClick}><FontAwesomeIcon icon={faThumbsUp} /></button>
             <button onClick={handleDislikeClick}><FontAwesomeIcon icon={faThumbsDown} /></button>
           </div>
           {/* Agrega un enlace a la página de detalles */}
           <Link to={`/${media.media_type}/${media.id}`}>Ver detalles</Link>
         </div>
+        </div>
       ) : (
         <p>Cargando recomendación...</p>
       )}
     </div>
+    
   );
 };
 
