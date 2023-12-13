cf5d5e707446ec376160b0aa32fe349e1e81e8d9
Sabrina Hill <brinalynn@hotmail.com>
2023-12-13 15:47:26 +0000

    updated all code

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import { Route, Routes } from 'react-router-dom';
-import { AuthContextProvider } from './context/AuthContext';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Account from './pages/Account';
-import ProtectedRoute from './components/ProtectedRoute';
-import Login from './pages/login';
-import Navbar from './components/NavBar';
-import MovieDetails from './components/MovieDetails';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <AuthContextProvider>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/movies/:id' element={<MovieDetails />}/>
-         
-          <Route
-            path='/account'
-            element={
-              <ProtectedRoute>
-                <Account />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </AuthContextProvider>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
